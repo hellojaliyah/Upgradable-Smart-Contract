1efac7592660aac3435221ea75e10556e2b46a12
hellojaliyah <jaliyahdharrison@gmail.com>
2023-09-12 04:11:36 -0400

    added front end.

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Navbar from "@/components/navbar";
+import Providers from "@/components/providers";
+import { Inter } from 'next/font/google'
+import "./globals.css";
+import Footer from "@/components/footer";
+
+const inter = Inter({ subsets: ['latin'] })
+
+// Define the RootLayout function
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+            <div className="flex flex-col min-h-screen">
+              <Navbar />
+              {children}
+              <div className="grow" />
+              <Footer />
+            </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
